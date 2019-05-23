3c49f423effcc81cf99ffde1714fb5a76fe2cec5
PawelUstaszewski <47858598+PawelUstaszewski@users.noreply.github.com>
2019-05-23 14:17:25 +0200

    poprawione

--- zaj8/23.php
+++ zaj8/23.php
@@ -1,22 +1,26 @@
+
 <?php
-try {
-    $dsn = 'mysql:dbname=zwierzeta;host=127.0.0.1';
-    $user = 'root';
-    $password = '';
-    $db = new PDO($dsn, $user, $password);
-    $id = $_GET['id'];
-    $sql = 'delete from zwierzeta_domowe where id = :id';
-    $sth = $db->prepare($sql);
+	try
+	{
+	    $dsn = 'mysql:dbname=zwierzeta;host=127.0.0.1';
+	    $user = 'root';
+	    $password = '';
+	    $db = new PDO($dsn, $user, $password);
+	    $id = $_GET['id'];
+	    $sql = 'DELETE FROM zwierzeta_domowe1 WHERE id = :id';
+	    $sth = $db->prepare($sql);
 
-    $sth->bindParam(':id', $id);
-    if($sth->execute()) {
-        echo "usuniete";
-    }
-    else echo "brak";
-
-
-}
-catch (PDOException $e) {
-    echo 'Connection failed';
-}
-?>
+	    $sth->bindParam(':id', $id);
+      print_r($sth->execute());
+      /*
+	    if($sth->execute()) {
+	        echo "Usunęło.";
+	    }
+	    else echo "Nie usunęło.";
+*/
+  }
+	catch (PDOException $e)
+	{
+	    echo 'Connection failed';
+	}
+  ?>
